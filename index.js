cce9904cfce747a7b22187f9da02a65a5f64ca27
unknown <benjaminprogrammering@gmail.com>
2020-11-09 14:16:45 +0100

    Update 6

--- index.js
+++ index.js
@@ -3,13 +3,12 @@
 const input = document.getElementById("user-input");
 const message = document.getElementById("message");
 const restartBtn = document.getElementsByTagName("button")[0];
-const high_score = document.getElementById("high_score");
-let secretNumber = randomInt(101);
-let guesses = [];
+const highScore = document.getElementById("high_score");
 const numbers = /^[0-9]+$/;
 const correctGuessText = "You are correct!";
 let wrongAttempts = 0;
-
+let guesses = [];
+let secretNumber = randomInt(101);
 //call on our functions :)
 setHighScore();
 setDefaultMessage();
@@ -44,16 +43,14 @@ function setMessage(userGuess) {
     if (userGuess > secretNumber) {
         message.innerHTML = "Too high!";
         guesses.push({ guess: userGuess, tooHigh: true });
-    }
-    else if (userGuess < secretNumber) {
+    } else if (userGuess < secretNumber) {
         message.innerHTML = "Too low!";
         guesses.push({ guess: userGuess, tooHigh: false });
-    }
-    else {
+    } else {
         message.innerHTML = correctGuessText + " Wrong Attempts: " + wrongAttempts + "!<br>Attempts: "
         for (let x = 0; x < guesses.length; x++) {
-            message.innerHTML += "<span style='color: " + (guesses[x].tooHigh ? "red" : "green")
-                + "';>" + guesses[x].guess + ((x < guesses.length - 1) ? "," : "") + " </span>";
+            message.innerHTML += "<span style='color: " + (guesses[x].tooHigh ? "red" : "green") +
+                "';>" + guesses[x].guess + ((x < guesses.length - 1) ? "," : "") + " </span>";
         }
         let newHighScore = false;
         if (!localStorage.getItem("best_result") || localStorage.getItem("best_result") > wrongAttempts) {
@@ -71,12 +68,12 @@ function setMessage(userGuess) {
 }
 
 function registerListeners() {
-    document.addEventListener("keyup", function (e) {
+    document.addEventListener("keyup", function(e) {
         if (e.key === "Enter") {
             setMessage(getUserGuess());
         }
     });
-    restartBtn.addEventListener("click", function () {
+    restartBtn.addEventListener("click", function() {
         resetGame();
     })
 }
